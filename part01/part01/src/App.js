c0055ccbdd34dbf1b41e6baeb3964a10909f6d72
The Handy Owl <thehandyowl.io@gmail.com>
2021-02-03 00:19:31 +0100

    Playing with HTML and external components

--- part01/part01/src/App.js
+++ part01/part01/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
+
+// Esta forma de declarar el componente, y el componente en forma de función son equivalentes
+
+const  Description = () => {
+    return (
+        <p>Lorem ipsum dolor sit amet.</p>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className = "App">
+            <Message color='red' message='Hola Mundo!' />
+            <Message color='yellow' message='Haciendo pruebas desde React!' />
+            <Message color='green' message='Para todo el mundo' />
+            <Description />
+        </div>
+    );
 }
 
 export default App;
