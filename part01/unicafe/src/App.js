902efa0161c314275760b8c78d9a7424029ae2c9
The Handy Owl <thehandyowl.io@gmail.com>
2021-02-05 16:37:01 +0100

    Exercise 1.8

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -1,34 +1,45 @@
 import { useState } from 'react';
 import './App.css';
 
-const Button = ( { text, handleClick } ) => <button onClick={handleClick}>{text}</button>
+const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>
+
+const StatisticLine = ({ text, calc }) => <p>{text}: <span style={{ color: "blue", fontWeight: "bold" }}>{calc}</span></p>
+
+//const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({good, neutral, bad}) => {
+  const statisticsAll = good + neutral + bad
+  const statisticsAvg = isNaN(((good - bad) / (good + neutral + bad)).toFixed(2)) ? "0" : ((good - bad) / (good + neutral + bad)).toFixed(2)
+  const statisticsPositive = `${isNaN((good / (good + neutral + bad)).toFixed(2)) ? 0 : (good / (good + neutral + bad)).toFixed(2)}%`
+
+  return (
+    <>
+      <h2>Statistics</h2>
+      <StatisticLine text='Good' calc={good} />
+      <StatisticLine text='Neutral' calc={neutral} />
+      <StatisticLine text='Bad' calc={bad} />
+      <StatisticLine text='All' calc={statisticsAll} />
+      <StatisticLine text='Average' calc={statisticsAvg} />
+      <StatisticLine text='Positive' calc={statisticsPositive} />
+    </>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  
-  const addGood = (good) => setGood(good+1)
-  const addNeutral = (neutral) => setNeutral(neutral+1)
-  const addBad = (bad) => setBad(bad+1)
 
-  const statisticsAll = good + neutral + bad
-  const statisticsAvg = ( (good - bad ) / (good + neutral + bad) ).toFixed(2)
-  const statisticsPositive = `${(good / ( good + neutral + bad )).toFixed(2)}%`
+  const addGood = (good) => setGood(good + 1)
+  const addNeutral = (neutral) => setNeutral(neutral + 1)
+  const addBad = (bad) => setBad(bad + 1)
 
   return (
     <div className="App">
       <h2>Give feedback</h2>
-      <Button text='good' handleClick={ () => addGood(good) } />
-      <Button text='neutral' handleClick={ () => addNeutral(neutral) } />
-      <Button text='bad' handleClick={ () => addBad(bad) } />
-      <h2>Statistics</h2>
-      <p>Good: { good }</p>
-      <p>Neutral: { neutral }</p>
-      <p>Bad: { bad }</p>
-      <p>All: { statisticsAll }</p>
-      <p>Average: { statisticsAvg }</p>
-      <p>Positive: { statisticsPositive }</p>
+      <Button text='good' handleClick={() => addGood(good)} />
+      <Button text='neutral' handleClick={() => addNeutral(neutral)} />
+      <Button text='bad' handleClick={() => addBad(bad)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
