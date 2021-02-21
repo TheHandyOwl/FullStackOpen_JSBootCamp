ee95ef821cd519a0272946662d50c04e6820c553
The Handy Owl <thehandyowl.io@gmail.com>
2021-02-21 11:32:31 +0100

    Exercise 2.3

--- part02/courseinfo/src/Total.js
+++ part02/courseinfo/src/Total.js
@@ -1,3 +1,4 @@
-const Total = ( {parts} ) => <p>Total of { parts.map( part => part.exercises).reduce( (accumulator, currentValue) => accumulator + currentValue ) } exercises</p>
+//const Total = ( {parts} ) => <p>Total of { parts.map( part => part.exercises).reduce( (accumulator, currentValue) => accumulator + currentValue ) } exercises</p>
+const Total = ( {parts} ) => <p>Total of { parts.reduce( (accumulator, currentValue) => (accumulator + currentValue.exercises), 0 ) } exercises</p>
 
 export default Total;
